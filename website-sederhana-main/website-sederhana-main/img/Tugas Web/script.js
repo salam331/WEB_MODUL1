790a02ce0e856e3171e83eb25c18e6a2dd2d3a23
salam331 <156354978+salam331@users.noreply.github.com>
2024-10-28 22:19:10 +0700

    Delete website-sederhana-main/website-sederhana-main/img/Tugas Web directory

--- website-sederhana-main/website-sederhana-main/img/Tugas Web/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('learn-more').addEventListener('click', function () {
-        document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
-    });
-});
